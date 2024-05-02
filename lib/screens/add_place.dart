57ea5be30e27506aadffb3071fb1fba1669eface
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-05-02 23:47:31 +0100

    created an image widget and installed the image picker from pub.dev

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,5 +1,6 @@
 
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -51,6 +52,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
               ),
+
+              const SizedBox(height: 13),
+
+              ImageInput(),
 
               const SizedBox(height: 16),
 
