3b2132b999787a5e5f50316b5fc8259f1d5cd46e
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-05-06 18:44:55 +0100

    place details screen

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -25,11 +25,13 @@ class PlacesScreen extends ConsumerWidget {
 
         },
           ),
-      
         ]
       ),
 
-      body: PlacesList(places: userPlaces,),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: PlacesList(places: userPlaces,),
+      ),
     );
   }
 }
